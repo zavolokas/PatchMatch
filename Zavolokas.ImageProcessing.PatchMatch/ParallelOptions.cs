9c60c0eefd133db76de6c6332d4b5924d71f7e4c
zavolokas <sergey.zavoloka@gmail.com>
2018-01-23 07:49:12 +0100

    Comments for the settings

--- Zavolokas.ImageProcessing.PatchMatch/ParallelOptions.cs
+++ Zavolokas.ImageProcessing.PatchMatch/ParallelOptions.cs
@@ -1,8 +1,24 @@
 ﻿namespace Zavolokas.ImageProcessing.PatchMatch
 {
+    /// <summary>
+    /// Holds settings for the parallel processing.
+    /// </summary>
     public class ParallelOptions
     {
+        /// <summary>
+        /// Amount of elements(pixels) that should be processed in one thread. To control the overhead.
+        /// </summary>
+        /// <value>
+        /// The not dividable minimum amount elements.
+        /// </value>
         public int NotDividableMinAmountElements { get; set; } = 80;
+
+        /// <summary>
+        /// Gets or sets the threads count (good when matches to the amount of processor's cores).
+        /// </summary>
+        /// <value>
+        /// The threads count.
+        /// </value>
         public byte ThreadsCount { get; set; } = 4;
     }
 }
--- Zavolokas.ImageProcessing.PatchMatch/PatchMatchSettings.cs
+++ Zavolokas.ImageProcessing.PatchMatch/PatchMatchSettings.cs
@@ -3,25 +3,65 @@
 namespace Zavolokas.ImageProcessing.PatchMatch
 {
 
+    /// <summary>
+    /// Holds settings to control PatchMatch algorithm.
+    /// </summary>
+    /// <seealso cref="Zavolokas.ImageProcessing.PatchMatch.ParallelOptions" />
+    /// <seealso cref="System.ICloneable" />
     public sealed class PatchMatchSettings : ParallelOptions, ICloneable
     {
+        /// <summary>
+        /// The patch size (should be odd).
+        /// </summary>
         public byte PatchSize = 11;
+
+        /// <summary>
+        /// The iterations amount.
+        /// </summary>
         public byte IterationsAmount = 5;
+
+        /// <summary>
+        /// The premerge iterations amount.
+        /// </summary>
         public byte PremergeIterationsAmount = 2;
+
+        /// <summary>
+        /// The radius of the area for the random search phase of the algorithm.
+        /// </summary>
         public double RandomSearchRadius = 2;
+
+        /// <summary>
+        /// The random search alpha.
+        /// </summary>
         public double RandomSearchAlpha = 0.5;
+
+        /// <summary>
+        /// The minimum size of the window to search in.
+        /// </summary>
         public int MinSearchWindowSize = 240;
-        public double StablePixelsProcent = 1;
+
+        /// <summary>
+        /// The iterations amount for the processing within a markup.
+        /// </summary>
         public byte MarkupIterationsAmount = 5;
+
+        public double StablePixelsProcent = 1;
         public int MaxAllowedTuIterations = int.MaxValue;
         public bool RestrictTuIterations = false;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatchMatchSettings"/> class.
+        /// </summary>
         public PatchMatchSettings()
         {
             ThreadsCount = (byte)Environment.ProcessorCount;
             NotDividableMinAmountElements = 80;
         }
 
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns></returns>
         public object Clone()
         {
             return new PatchMatchSettings
@@ -34,10 +74,11 @@ namespace Zavolokas.ImageProcessing.PatchMatch
                 MinSearchWindowSize = MinSearchWindowSize,
                 ThreadsCount = ThreadsCount,
                 NotDividableMinAmountElements = NotDividableMinAmountElements,
-                StablePixelsProcent = StablePixelsProcent,
                 MarkupIterationsAmount = MarkupIterationsAmount,
+
                 MaxAllowedTuIterations = MaxAllowedTuIterations,
                 RestrictTuIterations = RestrictTuIterations,
+                StablePixelsProcent = StablePixelsProcent,
             };
         }
     }
