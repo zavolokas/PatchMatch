f49ed44ce1368c8222e1243e78dc799577f7dfbb
zavolokas <sergey.zavoloka@gmail.com>
2018-03-04 07:58:04 +0100

    new sample with real use case of donors
    (has a dependency on Inpainting)

---
+++ Samples/DonorsAdvanced/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zavolokas.ImageProcessing.PatchMatch;
+using Zavolokas.Structures;
+using Zavolokas.GdiExtensions;
+using Zavolokas.ImageProcessing.Inpainting;
+using Zavolokas.Utils.Processes;
+
+namespace DonorsAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            const string imagesPath = @"../../../images";
+
+            var donorNames = new string[0];
+
+            const string imageName = "t061.jpg";
+            const string markupName = "m061.png";
+            donorNames = new[] { "d0611.png", "d0612.png" };
+
+            var imageArgb = OpenArgbImage(Path.Combine(imagesPath, imageName));
+            var w = imageArgb.Width;
+            var h = imageArgb.Height;
+            var markupArgb = OpenArgbImage(Path.Combine(imagesPath, markupName));
+            var donors = new List<ZsImage>();
+            if (donorNames.Any())
+            {
+                donors.AddRange(donorNames.Select(donorName => OpenArgbImage(Path.Combine(imagesPath, donorName))));
+            }
+
+
+            var pyramidBuilder = new PyramidBuilder();
+            pyramidBuilder.Init(imageArgb, markupArgb);
+            foreach (var donor in donors)
+            {
+                pyramidBuilder.AddDonorMarkup(donor);
+            }
+            var pyramid = pyramidBuilder.Build(2);
+
+            var destImage = pyramid.GetImage(0);
+            var srcImage = destImage;
+            var map = pyramid.GetMapping(0);
+
+            const byte patchSize = 11;
+            var nnf = new Nnf(destImage.Width, destImage.Height, srcImage.Width, srcImage.Height, patchSize);
+            // Prepage setting for the PM algorithm
+            var settings = new PatchMatchSettings
+            {
+                PatchSize = patchSize
+            };
+            var calculator = ImagePatchDistance.Cie76;
+
+            var patchMatchNnfBuilder = new PatchMatchNnfBuilder();
+
+            // Create the nnf for the small variant of the images
+            // with a couple of iterations.
+            patchMatchNnfBuilder.RunRandomNnfInitIteration(nnf, destImage, srcImage, settings, calculator, map);
+            patchMatchNnfBuilder.RunBuildNnfIteration(nnf, destImage, srcImage, NeighboursCheckDirection.Forward, settings, calculator, map);
+            patchMatchNnfBuilder.RunBuildNnfIteration(nnf, destImage, srcImage, NeighboursCheckDirection.Backward, settings, calculator, map);
+            patchMatchNnfBuilder.RunBuildNnfIteration(nnf, destImage, srcImage, NeighboursCheckDirection.Forward, settings, calculator, map);
+            patchMatchNnfBuilder.RunBuildNnfIteration(nnf, destImage, srcImage, NeighboursCheckDirection.Backward, settings, calculator, map);
+            patchMatchNnfBuilder.RunBuildNnfIteration(nnf, destImage, srcImage, NeighboursCheckDirection.Forward, settings, calculator, map);
+
+            // Restore dest image from the NNF and source image.
+            nnf
+                .RestoreImage(srcImage, 3, patchSize)
+                .FromLabToRgb()
+                .FromRgbToBitmap()
+                .SaveTo(@"..\..\restored.png", ImageFormat.Png);
+
+            // Convert the NNF to an image, save and show it
+            nnf
+                .ToRgbImage()
+                .FromRgbToBitmap()
+                .SaveTo(@"..\..\nnf.png", ImageFormat.Png)
+                .ShowFile();
+
+            sw.Stop();
+
+            Console.WriteLine($"Elapsed time: {sw.Elapsed}");
+            Console.WriteLine($"PatchMatchPipeline processing is finished.");
+        }
+
+        private static ZsImage GetLabImage(string basePath, string fileName)
+        {
+            ZsImage result;
+            var destFilePath = Path.Combine(basePath, fileName);
+            using (var destBitmap = new System.Drawing.Bitmap(destFilePath))
+            {
+                result = destBitmap.ToRgbImage()
+                    .FromRgbToLab();
+            }
+            return result;
+        }
+
+        private static Area2D GetArea2D(string basePath, string fileName)
+        {
+            Area2D result;
+            var destFilePath = Path.Combine(basePath, fileName);
+            using (var destBitmap = new Bitmap(destFilePath))
+            {
+                result = destBitmap.ToArea();
+            }
+            return result;
+        }
+
+        private static ZsImage OpenArgbImage(string path)
+        {
+            ZsImage image;
+            using (var imageBitmap = new Bitmap(path))
+            {
+                image = imageBitmap.ToArgbImage();
+            }
+            return image;
+        }
+    }
+}
